f660063af7fc80f7b4b47bad01421119e6f9c648
Nikita Goldashevsky <nikitagoldashevsky@yandex.ru>
2024-12-08 17:35:12 +0300

    Remove unused namespace directives from several files

--- SQLite-Insight/View/SelectDialog.xaml.cs
+++ SQLite-Insight/View/SelectDialog.xaml.cs
@@ -1,7 +1,5 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls;
 
 namespace SQLite_Insight.View
 {
